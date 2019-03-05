4bfd63bb465aaa775bd3a6c0c361b69a83e24374
AmberZheng0621 <u1145083@utah.edu>
2019-03-04 20:15:01 -0700

    add group project

---
+++ ChessBrowser/ChessTools/ChessGame.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessTools
+{
+    public class ChessGame
+    {
+        private String Event;
+        public String GetEventName()
+        {
+            return this.Event;
+        }
+        public void SetEventName(String Eve)
+        {
+            this.Event = Eve;
+        }
+        private String Site;
+        public String GetSite()
+        {
+            return this.Site;
+        }
+        public void SetSite(String site)
+        {
+            this.Site = site;
+        }
+
+        private String EventDate;
+        public String GetDate()
+        {
+            return this.EventDate;
+        }
+        public void SetDate(String date)
+        {
+            this.EventDate = date;
+        }
+        private String Black;
+        public String GetBlackName()
+        {
+            return this.Black;
+        }
+        public void SetBlackName(String black)
+        {
+            this.Black = black;
+        }
+        private String White;
+        public String GetWhiteName()
+        {
+            return this.White;
+        }
+        public void SetWhiteName(String white)
+        {
+            this.White = white;
+        }
+        private String WhiteElo;
+        public String GetWhiteElo()
+        {
+            return this.WhiteElo;
+        }
+        public void SetWhiteElo(String whiteelo)
+        {
+            this.WhiteElo = whiteelo;
+        }
+        private String BlackElo;
+        public String GetBlackElo()
+        {
+            return this.BlackElo;
+        }
+        public void SetBlackElo(String blackelo)
+        {
+            this.BlackElo = blackelo;
+        }
+        private String Result;
+        public String GetResult()
+        {
+            return this.Result;
+        }
+        public void SetResult(String result)
+        {
+            this.Result = result;
+        }
+        private String moves;
+        public String Getmoves()
+        {
+            return this.moves;
+        }
+        public void Setmoves(String move)
+        {
+            this.moves = move;
+        }
+
+
+    }
+}
---
+++ ChessBrowser/ChessTools/PGNReader.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ChessTools
+{
+    
+    class PGNReader
+    {
+       public static void main(String[] args)
+        {
+            ReadPGN("kb1.pgn");
+        }
+        public static List<ChessGame> ReadPGN(String path)
+        {
+            List<ChessGame> games= new List<ChessGame>();
+           if(!File.Exists(path))
+            {
+                String[] readText = File.ReadAllLines(path);
+                int i = 0;
+                while(i<readText.Length)
+                {
+                    String each = readText[i];
+                    ChessGame game = new ChessGame();
+                    while (each.StartsWith("["))
+                        {
+                           
+                            if (each.StartsWith("[EventDate"))
+                            {
+                                game.SetDate(each.Substring(12, each.IndexOf("]") - 2));
+                            i++;
+                            each = readText[i];
+                                continue;
+                            }
+
+                            if (each.StartsWith("[Event"))
+                            {
+                                game.SetEventName(each.Substring(8, each.IndexOf("]") - 2));
+                            i++;
+                            each = readText[i];
+                            continue;
+                            }
+                            if (each.StartsWith("[Site"))
+                            {
+                                game.SetSite(each.Substring(7, each.IndexOf("]") - 2));
+                            i++;
+                            each = readText[i];
+                            continue;
+                            }
+
+                            if (each.StartsWith("[WhiteElo"))
+                            {
+                                game.SetWhiteElo(each.Substring(11, each.IndexOf("]") - 2));
+                            i++;
+                            each = readText[i];
+                            continue;
+                            }
+                            if (each.StartsWith("[BlackElo"))
+                            {
+                                game.SetBlackElo(each.Substring(11, each.IndexOf("]") - 2));
+                            i++;
+                            each = readText[i];
+                            continue;
+                            }
+                            if (each.StartsWith("[White"))
+                            {
+                                game.SetWhiteName(each.Substring(8, each.IndexOf("]") - 2));
+                            i++;
+                            each = readText[i];
+                            continue;
+                            }
+                            if (each.StartsWith("[Black"))
+                            {
+                                game.SetBlackName(each.Substring(8, each.IndexOf("]") - 2));
+                            i++;
+                            each = readText[i];
+                            continue;
+                            }
+                            if (each.StartsWith("[Result"))
+                            {
+                                game.SetResult(each.Substring(9, each.IndexOf("]") - 2));
+                            i++;
+                            each = readText[i];
+                            continue;
+                            }
+                            else
+                        {
+                            i++;
+                        }
+                    }
+                    String move = "";
+                    i++;
+                    while (String.IsNullOrEmpty(each))
+                    {
+                        move = move + each;
+                        i++;
+                    }
+                    game.Setmoves(move);
+                    games.Add(game);
+                }
+                
+
+                }
+
+            
+
+            return null;
+        }
+        
+    }
+}
